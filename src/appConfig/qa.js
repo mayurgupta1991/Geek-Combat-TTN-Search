0b42084704e5ddc15628301038f1c5baea5d6b1e
Mayur Gupta <mayur.gupta@tothenew.com>
2017-10-08 03:46:47 +0530

    Updated Google key

--- src/appConfig/qa.js
+++ src/appConfig/qa.js
@@ -1,7 +1,7 @@
 export default {
     BASE: 'http://welcome-api.qa3.tothenew.net/api/v1/',
     AUTH: 'http://welcome-api.qa3.tothenew.net/',
-    GOOGLE_CLIENT_ID: '12000940573-9dvbfsmo3teud0djri92igp3amdnu357.apps.googleusercontent.com',
+    GOOGLE_CLIENT_ID: '195840081711-7bgab0a0e3u15j1p9icmcig6d68i6vhm.apps.googleusercontent.com',
     COOKIE_EXPIRY_TIME: 60 * 60 * 12 * 1,
     AWS_CONFIG: {
         region: 'ap-south-1',
